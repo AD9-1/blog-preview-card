1ba3429c6c62e20d04f0227c85d00437e096b8ee
Aparna Dhara <84341093+AD9-1@users.noreply.github.com>
2024-06-12 00:52:31 -0700

    blog started

---
+++ src/components/Blog/Blog.js
@@ -0,0 +1,30 @@
+import React from "react";
+import "./Blog.scss";
+import Banner from "../assets/illustration-article.svg";
+import author from "../assets/image-avatar.webp";
+const Blog = () => {
+  return (
+    <div className="Blog">
+      <div className="Blog__card">
+        <div className="Blog__card__image">
+          <img src={Banner} alt="" />
+        </div>
+        <div className="Blog__card__info">
+          <h4>learning</h4>
+          <p className="date">Published 21 Dec 2023</p>
+          <h2>HTML & CSS foundations</h2>
+          <p className="statement">
+            These languages are backbone of every website,defining
+            structure,content, and presentation
+          </p>
+        </div>
+        <div className="Blog__card__author">
+          <img src={author} alt="" />
+          <h4>Greg Hopper</h4>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Blog;
